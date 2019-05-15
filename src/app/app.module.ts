140fc02a5348915225ed67d3ebe2071ab4e7409b
mohanacchu <mohangowda1994@gmail.com>
2019-05-15 23:51:01 +0530

    new pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { HeaderComponent } from './Header/header.component';
 import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './Shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './Shopping/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
